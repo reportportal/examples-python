59c9920abc730b4d93a754b40983b4b5deb5948b
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-03-31 12:23:34 +0300

    Removing additional run scripts

--- robotframework/run.sh
+++ robotframework/run.sh
@@ -5,4 +5,6 @@ robot --listener robotframework_reportportal.listener \
       --variable RP_UUID:"your_user_uuid" \
       --variable RP_ENDPOINT:"http://localhost:8080" \
       --variable RP_LAUNCH:"Robot Framework" \
-      --variable RP_PROJECT:"default_personal" ./robot
+      --variable RP_PROJECT:"default_personal" \
+      --variable RP_LAUNCH_ATTRIBUTES:"tag key1:value1 key2:value2" \
+      --variable RP_LAUNCH_DOC:"My test launch" ./robot
