ff46a2ec21005ec35ec2a63c0f196b65188ba25a
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-12-02 17:23:25 +0300

    More examples

---
+++ pytest/tests/test_description.py
@@ -0,0 +1,7 @@
+
+def test_description(rp_logger):
+    """
+    This is my test description which appears on ReportPortal
+    """
+    rp_logger.info("A test with description")
+    assert True is True
