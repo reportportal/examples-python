c8e1f7539fc3836709bd4f54887b8efe003df4f0
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-12-03 13:45:06 +0300

    A test description add

--- pytest/tests/test_launch_url_get.py
+++ pytest/tests/test_launch_url_get.py
@@ -1,5 +1,10 @@
 
 def test_launch_url_get(rp_logger, rp_launch_id, rp_endpoint, rp_project):
+    """
+    This is a test which gets Launch ID, Report Portal endpoint and project
+    from fixtures in `conftest.py` and prints the result in logs, both: in
+    console and on Report Portal
+    """
     rp_logger.info("Got launch ID: %s", rp_launch_id)
     rp_logger.info("Launch URL: %s/ui/#%s/launches/all/%s", rp_endpoint,
                    rp_project, rp_launch_id)
