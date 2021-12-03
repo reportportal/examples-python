d8e2b4f1e1d4424d4bf024c19f93e3a71351d106
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-12-03 13:33:53 +0300

    Multiple custom attribute example

---
+++ pytest/tests/test_custom_attributes.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.scope("smoke")
+@pytest.mark.scope("regression")
+def test_custom_attributes_report(rp_logger):
+    """
+    This is a test with multiple custom markers which shall appear on
+    ReportPortal on test's item as different attributes
+    """
+    rp_logger.info("A test with multiple custom attributes")
+    assert True is True
