0ddfffedc60503bd20a60202b97975cfd30c53af
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-12-06 12:01:19 +0300

    Ignored attribute example add

--- pytest/tests/test_custom_attributes.py
+++ pytest/tests/test_custom_attributes.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.mark.scope("smoke")
 @pytest.mark.scope("regression")
+@pytest.mark.ignored_attribute("test")
 def test_custom_attributes_report(rp_logger):
     """
     This is a test with multiple custom markers which shall appear on
