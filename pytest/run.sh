903f104c7aff37ef4567c7d5f5829e3e6042c3c1
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-07-05 12:38:09 +0300

    Revert "Update run script"
    This reverts commit d1cb0a758ddb616124d2a8ae26e82faf79350c5f.

--- pytest/run.sh
+++ pytest/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pytest -sv --reportportal -m "not command_skip" -n 2 tests | tee /dev/tty | sed -rn 's/Report Portal Launch UUID: ([^\\r\\n]+)/LAUNCH_UUID=\1/ w launch.env'
+pytest --reportportal -m "not command_skip" tests
