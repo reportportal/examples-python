d1cb0a758ddb616124d2a8ae26e82faf79350c5f
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-07-05 12:24:57 +0300

    Update run script

--- pytest/run.sh
+++ pytest/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pytest --reportportal -m "not command_skip" tests
+pytest -sv --reportportal -m "not command_skip" -n 2 tests | tee /dev/tty | sed -rn 's/Report Portal Launch UUID: ([^\\r\\n]+)/LAUNCH_UUID=\1/ w launch.env'
