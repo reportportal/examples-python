63d377978bab9f7cf3f3e9396af5f2c7b35a0f29
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-11-29 16:11:39 +0300

    Update run script examples

--- robotframework/run.sh
+++ robotframework/run.sh
@@ -3,7 +3,7 @@
 # Replace runner variables with your variables
 robot --skiponfailure not_ready \
       --listener robotframework_reportportal.listener \
-      --variable RP_API_KEY:"your_rp_access_token" \
+      --variable RP_API_KEY:"your_rp_api_key" \
       --variable RP_ENDPOINT:"http://localhost:8080" \
       --variable RP_LAUNCH:"Robot Framework" \
       --variable RP_PROJECT:"default_personal" \
